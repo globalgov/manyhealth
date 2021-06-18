6d87c8f8f01ceeeb3856e8d71b247db7e89061ab
EstherPeev <esther.peev@graduateinstitute.ch>
2021-06-18 16:24:58 +0200

    Added HEIDI dataset and exported into agreement database

---
+++ R/qData-agreements.R
@@ -0,0 +1,11 @@
+#' agreements database documentation
+#'
+#' @format The agreements database is a list that contains the
+#' following 1 datasets: HEIDI.
+#' For more information and references to each of the datasets used,
+#' please use the `data_source()`, `data_contrast()`, and `data_evolution()`
+#' functions.
+#'\describe{
+#' \item{HEIDI: }{A dataset with 2280 observations and the following 3 variables: ID, Title, Beg.}
+#' }
+ "agreements"
