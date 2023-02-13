62b9d2e6a1e22c6bffce58babc0b42f83d618011
Jael Tan <jael.tan@graduateinstitute.ch>
2023-02-13 11:42:23 +0100

    Replaced 'messydt' with new date class 'mdate' in test scripts

--- tests/testthat/test_GHHR_REF.R
+++ tests/testthat/test_GHHR_REF.R
@@ -10,7 +10,7 @@ test_that("missing observations are reported correctly", {
   expect_false(any(grepl("n\\.a\\.$", references[["GHHR_REF"]])))
 })
 
-# Date columns should be in messydt class
+# Date columns should be in mdate class
 test_that("Columns are not in date, POSIXct or POSIXlt class", {
   expect_false(any(lubridate::is.Date(references[["GHHR_REF"]])))
   expect_false(any(lubridate::is.POSIXct(references[["GHHR_REF"]])))
